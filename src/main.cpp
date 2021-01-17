fe99f87dcef7de6febc7284b06f134ee86b95f61
Michael Puening <mpuening@gmail.com>
2021-01-17 09:20:05 -0500

    Merge branch 'master' into cpp
    # Conflicts:
    #	src/Makefile.am
    #	src/main.c

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <config.h>
 #include <iostream>
 
-#include "../include/hello.h"
+#include "hello.h"
+
+extern "C" {
+#include "util.h"
+}
 
 using namespace HELLO;
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	cout << "Running " PACKAGE_STRING "." << endl;
+        printf("Util version: %s\n", util_version());
 	Hello hello;
 	hello.runApplication();
 	return 0;
 }
+
